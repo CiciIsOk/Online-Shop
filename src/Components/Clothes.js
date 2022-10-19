b6d044bae1e9dff1a545bfd49704723488a82cb9
CiciIsOk <cici.iobashvili2000@gmail.com>
2022-10-19 11:14:19 +0400

    online shop

---
+++ src/Components/Clothes.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+export class Clothes extends Component {
+  render() {
+    return <div style={{ padding: "50px", margin: "20px" }}>Clothes</div>;
+  }
+}
+
+export default Clothes;
---
+++ src/Components/ItemPage.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+export class ItemPage extends Component {
+  render() {
+    return <div>ItemPage</div>;
+  }
+}
+
+export default ItemPage;
